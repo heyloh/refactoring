8219c8e159e29ca80be42856c4a22c9c1e5a3717
Lohana Torres <lt.leopoldo@gmail.com>
2023-04-17 08:53:10 -0300

    refactor(CHAPTER-01): first example - replace temp with query: play

--- chapters/01/statement.js
+++ chapters/01/statement.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     const thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -25,31 +25,36 @@ function statement(invoice, plays) {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+
   return result;
-}
 
-function amountFor(aPerformance, play) {
-  let result = 0;
+  function amountFor(aPerformance, play) {
+    let result = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return result;
   }
 
-  return result;
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 module.exports = statement;
